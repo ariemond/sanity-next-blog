787de89a6c131e2c369c5e3e51c6ed478a4e4368
Ariana <ariana.emond@zulualphakilo.com>
2022-03-28 16:50:18 -0400

    Building out dynamic post page

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -2,16 +2,104 @@ import { sanityClient, urlFor } from '../../sanity'
 import Header from '../../components/Header'
 import { GetStaticProps } from 'next'
 import { Post } from '../../typings'
+import PortableText from 'react-portable-text'
+import { useForm, SubmitHandler } from 'react-hook-form'
 
 interface Props {
   post: Post
 }
 
 function Post({ post }: Props) {
-  console.log(post)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
+
   return (
     <main>
       <Header />
+
+      <img
+        className="h-40 w-full object-cover"
+        src={urlFor(post.mainImage).url()!}
+      />
+
+      <article className="mx-auto max-w-3xl p-5">
+        <h1 className="mt-10 mb-3 text-3xl">{post.title}</h1>
+        <h2 className="mb-2 text-xl font-light text-gray-500">
+          {post.description}
+        </h2>
+
+        <div className="flex items-center space-x-2">
+          <img
+            className="h-10 w-10 rounded-full"
+            src={urlFor(post.author.image).url()!}
+            alt=""
+          />
+          <p className="text-sm font-extralight">
+            Blog post by{' '}
+            <span className="text-green-600">{post.author.name}</span> -
+            Published at {new Date(post._createdAt).toLocaleString()}
+          </p>
+        </div>
+
+        <div className="mt-10">
+          <PortableText
+            dataset={process.env.NEXT_PUBLIC_SANITY_DATASET!}
+            projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!}
+            content={post.body}
+            className=""
+            serializers={{
+              h1: (props: any) => (
+                <h1 className="my-5 text-2xl font-bold" {...props} />
+              ),
+              h2: (props: any) => (
+                <h2 className="my-5 text-xl font-bold" {...props} />
+              ),
+              li: ({ children }: any) => (
+                <li className="ml-4 list-disc">{children}</li>
+              ),
+              link: ({ href, children }: any) => (
+                <a className="text-blue-500 hover:underline" href={href}>
+                  {children}
+                </a>
+              ),
+            }}
+          />
+        </div>
+      </article>
+      <hr className="my-5 mx-auto max-w-lg border border-yellow-500"></hr>
+
+      <form className="mx-auto mb-10 flex max-w-2xl flex-col p-5">
+        <h3 className="text-sm text-yellow-500">Enjoyed this article?</h3>
+        <h4 className="text-3xl font-bold">Leave a comment below!</h4>
+        <hr className="mt-2 py-3"></hr>
+        <label className="mb-5 block">
+          <span className="text-gray-700">Name</span>
+          <input
+            className="form-input mt-1 block w-full rounded border py-2 px-3 shadow outline-none ring-yellow-500 focus:ring"
+            placeholder="John Appleseed"
+            type="text"
+          />
+        </label>
+        <label className="mb-5 block">
+          <span className="text-gray-700">Email</span>
+          <input
+            className="form-input mt-1 block w-full rounded border py-2 px-3 shadow outline-none ring-yellow-500 focus:ring"
+            placeholder="jappleseed@gmail.com"
+            type="text"
+          />
+        </label>
+        <label className="mb-5 block">
+          <span className="text-gray-700">Comment</span>
+          <textarea
+            className="form-textarea mt-1 block w-full rounded border py-2 px-3 shadow outline-none ring-yellow-500 focus:ring"
+            placeholder="Write your comment here..."
+            rows={8}
+          />
+        </label>
+      </form>
     </main>
   )
 }
@@ -71,5 +159,6 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     props: {
       post,
     },
+    revalidate: 60, // after 60secs, update the old cache
   }
 }
